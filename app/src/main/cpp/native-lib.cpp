9210638a0dc8e4b9ff3c24b787cf76ac4ffd7ad0
lambert <344042862@qq.com>
2025-06-09 16:06:06 +0800

    测试启用ollvm混淆成功

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,16 @@
 #include <jni.h>
 #include <string>
+#include <cstdlib>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_ollvmmodule_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    int a = atoi("sfvweg");
+    if(a == 0)
+        printf("%d",1);
+    else
+        printf("%d--f", 10);
     return env->NewStringUTF(hello.c_str());
 }
