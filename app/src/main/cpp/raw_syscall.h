75d7ea70b8acae0eabfc1141c2bfde06ff6e506a
lambert <344042862@qq.com>
2025-06-10 15:01:25 +0800

    1.初步实现raw_syscall内联汇编函数。 2.初步实现svc openat读apk，去解析签名。

---
+++ app/src/main/cpp/raw_syscall.h
@@ -0,0 +1,10 @@
+//
+// Created by wengzulan on 2025/6/10.
+//
+
+#ifndef OLLVMMODULE_RAW_SYSCALL_H
+#define OLLVMMODULE_RAW_SYSCALL_H
+
+extern "C" long raw_syscall(long __number, ...); //extern“C” 实现C++与C及其它语言的混合编程
+
+#endif //OLLVMMODULE_RAW_SYSCALL_H
---
+++ app/src/main/cpp/sign/CheckSign.h
@@ -0,0 +1,8 @@
+//
+// Created by wengzulan on 2025/6/10.
+//
+
+#ifndef OLLVMMODULE_CHECKSIGN_H
+#define OLLVMMODULE_CHECKSIGN_H
+
+#endif //OLLVMMODULE_CHECKSIGN_H
