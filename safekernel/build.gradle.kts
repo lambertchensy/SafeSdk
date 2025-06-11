933551cbebd0e7570ac9fb2abb3d432b216a2446
lambert <344042862@qq.com>
2025-06-11 18:31:38 +0800

    将build.gradle.kts中的ndk编译有关配置统一整理到一块

--- safekernel/build.gradle.kts
+++ safekernel/build.gradle.kts
@@ -9,14 +9,6 @@ android {
     defaultConfig {
         minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
-        ndk{
-            // 可选的 ABI 架构列表:
-            // arm64-v8a, armeabi-v7a, x86, x86_64
-            abiFilters.add("arm64-v8a")  // 64位ARM
-            abiFilters.add("armeabi-v7a") // 32位ARM
-            // abiFilters.add("x86")       // x86 (模拟器)
-            // abiFilters.add("x86_64")    // x86_64 (模拟器)
-        }
     }
 
     buildTypes {
@@ -32,17 +24,26 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
+    /**********************************
+     ******所有与ndk编译有关配置
+     **********************************/
+    //ndkVersion = "24.0.8215888"  //指定ndk版本，配合OLLVM-14.0.0(自带的clang版本14.0.1,替换后具有ollvm混淆的clang版本是14.0.0)
+    ndkVersion = "26.0.10792818"   // 指定ndk版本，配合DreamSoule的OLLVM-17.0.2 (自带的 clang version 17.0.2)
+    //ndkVersion = "27.2.12479018"  //指定ndk版本，配合buffcow的OLLVM-18.0.3
+    //ndkVersion = "28.0.13004108"  //指定ndk版本，配合buffcow的OLLVM-19.0.0
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
             version = "3.22.1"
         }
     }
-    //ndkVersion = "24.0.8215888"  //指定ndk版本，配合OLLVM-14.0.0(自带的clang版本14.0.1,替换后具有ollvm混淆的clang版本是14.0.0)
-    ndkVersion = "26.0.10792818"   // 指定ndk版本，配合DreamSoule的OLLVM-17.0.2 (自带的 clang version 17.0.2)
-    //ndkVersion = "27.2.12479018"  //指定ndk版本，配合buffcow的OLLVM-18.0.3
-    //ndkVersion = "28.0.13004108"  //指定ndk版本，配合buffcow的OLLVM-19.0.0
+    defaultConfig {
+        ndk {
+            // 可选的 ABI 架构列表: arm64-v8a, armeabi-v7a, x86, x86_64
+            abiFilters.addAll(listOf("armeabi-v7a","arm64-v8a"))  // 64位ARM
+        }
+    }
+
 }
 
 dependencies {
